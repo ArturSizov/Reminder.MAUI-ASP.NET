52d9bc5f04bacfbd5d99bcee0f5f88f9934ccb89
Artur Sizov <artur-sa@mail.ru>
2022-11-21 21:58:13 +0300

    Созданы необходимые модели

--- Reminder.Contracts/Models/Persons.cs
+++ Reminder.Contracts/Models/Persons.cs
@@ -8,6 +8,14 @@ namespace Reminder.Contracts.Models
 {
     public class Persons
     {
-
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Age { get; set; }
+        public int Days { get; set; }
+        public string Base64 { get; set; }
     }
 }
--- Reminder.Contracts/Models/Report.cs
+++ Reminder.Contracts/Models/Report.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Reminder.Contracts.Models
+﻿namespace Reminder.Contracts.Models
 {
     public class Report
     {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime Date { get; set; }
+        public string? Status { get; set; }
+        public string? Base64 { get; set; }
 
     }
 }
