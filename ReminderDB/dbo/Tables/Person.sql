5f1df0838c5fa1e2ac45077cb2be91731eadfb26
Artur Sizov <artur-sa@mail.ru>
2022-11-23 15:32:24 +0300

    Работа с БД. Создана процедура получение всех Person

--- ReminderDB/dbo/Tables/Person.sql
+++ ReminderDB/dbo/Tables/Person.sql
@@ -1,4 +1,12 @@
 ﻿CREATE TABLE [dbo].[Person]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NULL, 
+    [MiddleName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Position] NVARCHAR(50) NULL, 
+    [Birthday] DATETIME NULL, 
+    [Age] INT NULL, 
+    [Days] INT NULL, 
+    [Base64] TEXT NULL
 )
