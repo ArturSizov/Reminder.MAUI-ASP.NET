b14dda7c192fc2c7f1f783b6feee828a697830e9
Artur Sizov <artur-sa@mail.ru>
2023-02-21 22:15:21 +0300

    Завершил внешний вид FlyoutBehavior

--- Reminder/AppShell.xaml.cs
+++ Reminder/AppShell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Reminder.Views;
+﻿using Reminder.ViewModels;
+using Reminder.Views;
 
 namespace Reminder
 {
@@ -10,6 +11,18 @@ namespace Reminder
 
             Routing.RegisterRoute(nameof(AddPersonPage), typeof(AddPersonPage));
             Routing.RegisterRoute(nameof(DetailsPage), typeof(DetailsPage));
+
+            BindingContext = new AppShellViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+#if ANDROID
+            Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#FFC107"));
+
+            //Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.Window.AddFlags(Android.Views.WindowManagerFlags.LayoutAttachedInDecor);
+#endif
         }
     }
 }
---
+++ Reminder/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Reminder.ViewModels
+{
+    public class AppShellViewModel
+    {
+        public ICommand BackCommand => new DelegateCommand(async () =>
+        {
+            await Shell.Current.DisplayAlert("Message", "Messsa...", "Ok");
+        });
+    }
+}
